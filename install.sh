3eaa52acdcda52a67e172bbedcd519a4c87e6455
Torgeir Arnoy <torgeir@torgeir.us>
2024-04-19 14:39:58 +0200

    Fixed root check

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Customized to be system wide install, by Torgeir A 19.04.24
 
-# check for unzip before we continue
-if [ ! "$(command -v unzip)" ]; then
-  echo 'unzip is required but was not found. Install unzip first and then run this script again.' >&2
-  exit 1
+# check for root
+if [ `id -u` -ne 0 ]
+  then echo "Please run as root, as this script install system-wide."
+  exit
 fi
 
 mkdir -p /usr/share/nanorc/
@@ -13,3 +13,5 @@ echo "include /usr/share/nanorc/*.nanorc" >> /etc/nanorc
 
 chown -R root:root /usr/share/nanorc
 chmod -R 664 /usr/share/nanorc
+echo ""
+echo "Hunky dory, we're done!"
