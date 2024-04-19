d4713a0a176f5fe7e0b9f97da23ac59c8ab12979
Torgeir Arnoy <torgeir@torgeir.us>
2024-04-19 14:52:27 +0200

    Added check if install.sh is run from this folder #TorgeirA

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ if [ `id -u` -ne 0 ]
   exit
 fi
 
+# check if there is over 100 .nanorc files in current folder
+num_of_nanorc=$(ls | grep .nanorc | wc -l)
+if [ $num_of_nanorc -lt 100 ]
+  then echo "Seems like there is missing .nanorc files in current folder. Ensure that you run this script from its source folder"
+  exit 0
+fi
+
 mkdir -p /usr/share/nanorc/
 cp *.nanorc /usr/share/nanorc/
 echo "include /usr/share/nanorc/*.nanorc" >> /etc/nanorc
